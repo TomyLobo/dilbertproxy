7cecaa43946f240851f3cb01ab43a37a42fcf6b7
TomyLobo <tomylobo@nurfuerspam.de>
2015-12-18 23:30:25 +0100

    Less indentation

--- dilbertproxy.rb
+++ dilbertproxy.rb
@@ -5,18 +5,24 @@ require 'open-uri'
 require 'nokogiri'
 
 get '/dilbert' do
-  content_type 'text/xml'
-  open('http://feeds.dilbert.com/DilbertDailyStrip?format=xml') do |response|
-    xml_doc = Nokogiri::XML(response.read.to_s)
-
-    xml_doc.xpath('/atom:feed/atom:entry', :atom => "http://www.w3.org/2005/Atom").each do |entry|
-      url = entry.xpath('feedburner:origLink/text()').to_s
-      img = open(url) do |response|
-        html_doc = Nokogiri::HTML(response.read.to_s)
-        html_doc.css('.img-comic')[0]
-      end
-      entry.xpath('atom:content', :atom => "http://www.w3.org/2005/Atom")[0].content = img.to_s
-    end
-    xml_doc.to_s
+  xml = open('http://feeds.dilbert.com/DilbertDailyStrip?format=xml') do |response|
+    response.read.to_s
   end
+
+  xml_doc = Nokogiri::XML(xml)
+
+  xml_doc.xpath('/atom:feed/atom:entry', :atom => "http://www.w3.org/2005/Atom").each do |entry|
+    url = entry.xpath('feedburner:origLink/text()').to_s
+    html = open(url) do |response|
+      response.read.to_s
+    end
+
+    html_doc = Nokogiri::HTML(html)
+
+    img = html_doc.css('.img-comic')[0]
+    entry.xpath('atom:content', :atom => "http://www.w3.org/2005/Atom")[0].content = img.to_s
+  end
+
+  content_type 'text/xml'
+  xml_doc.to_s
 end
