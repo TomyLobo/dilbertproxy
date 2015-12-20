f8378a541d37f8cf3c0e8e2af6567ffbdf79683f
TomyLobo <tomylobo@nurfuerspam.de>
2015-12-20 23:12:49 +0100

    Simplified URL requests further.

--- dilbertproxy.rb
+++ dilbertproxy.rb
@@ -5,18 +5,14 @@ require 'open-uri'
 require 'nokogiri'
 
 get '/dilbert' do
-  xml = open('http://feeds.dilbert.com/DilbertDailyStrip?format=xml') do |response|
-    response.read.to_s
-  end
+  xml = open('http://feeds.dilbert.com/DilbertDailyStrip?format=xml').read.to_s
 
   xml_doc = Nokogiri::XML(xml)
   xml_doc.root.add_namespace_definition('atom', "http://www.w3.org/2005/Atom")
 
   xml_doc.xpath('/atom:feed/atom:entry').each do |entry|
     url = entry.xpath('feedburner:origLink/text()')[0].content
-    html = open(url) do |response|
-      response.read.to_s
-    end
+    html = open(url).read.to_s
 
     html_doc = Nokogiri::HTML(html)
 
