6b07295ba1af6699be889b2df4ee611b260297dd
TomyLobo <tomylobo@nurfuerspam.de>
2015-12-21 14:14:10 +0100

    Further simplified namespace definition.

--- dilbertproxy.rb
+++ dilbertproxy.rb
@@ -8,18 +8,17 @@ get '/dilbert' do
   xml = open('http://feeds.dilbert.com/DilbertDailyStrip?format=xml').read.to_s
 
   xml_doc = Nokogiri::XML(xml)
-  xml_doc.root.add_namespace_definition('atom', "http://www.w3.org/2005/Atom")
 
-  xml_doc.xpath('/atom:feed/atom:entry').each do |entry|
+  xml_doc.xpath('/xmlns:feed/xmlns:entry').each do |entry|
     url = entry.xpath('feedburner:origLink/text()')[0].content
     html = open(url).read.to_s
 
     html_doc = Nokogiri::HTML(html)
 
     img = html_doc.css('.img-comic')[0]
-    entry.xpath('atom:content')[0].content = img.to_s
+    entry.xpath('xmlns:content')[0].content = img.to_s
 
-    updated_node = entry.xpath('atom:updated')[0]
+    updated_node = entry.xpath('xmlns:updated')[0]
     updated_node.content = updated_node.content.gsub('23:59:59', '00:00:00')
   end
 
